73436553e6caefb14ee33c9aa8e0ce03b9a79115
Marlen-Chantalov <m.chantalov@mail.ru>
2023-12-07 18:11:20 +0300

    Сделана игра крестики-нолики

---
+++ tic_tac_toy.py
@@ -0,0 +1,72 @@
+def draw_board(board):
+    # создание игрового поля
+    for i in range(3):
+        print('|'.join(board[i]))
+        print('------')
+
+
+def ask_and_make_move(player, board):
+    # выбор координат для хода
+    x, y = input(f"{player}, введите координаты X Y, через проблел: ").strip().split()
+    x, y = int(x), int(y)
+    if (0 <= x <= 2) and (0 <= y <= 2) and (board[x][y] == " "):
+        board[x][y] = player
+    else:
+        print("Эта ячейка уже занята. Попробуйте снова.")
+
+
+def make_move(player, board, x, y):
+    # проверика на возможность хода
+    if board[x][y] != " ":
+        print("Клетка занята")
+        return False
+    # если клетка свободна, записать ход
+    board[x][y] = player
+    return True
+
+
+def check_win(player, board):
+    # проверка на совпаления ходов
+    for i in range(3):
+        # в строках
+        if board[i] == [player, player, player]:
+            return True
+        #  в столбцах
+        if board[0][i] == player and board[1][i] == player and board[2][i] == player:
+            return True
+    # проверка совпадений значений по диагонали
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    return False
+
+
+def tic_tac_toe():
+    # цикл вызова игры
+    while True:
+        board = [[" " for i in range(3)] for j in range(3)]
+        player = "X"
+        while True:
+            # отрисовка поля
+            draw_board(board)
+            # запрос хода
+            ask_and_make_move(player, board)
+            # проверка на победу
+            if check_win(player, board):
+                print(f"{player} выиграл!")
+                break
+            # проверка на  ничью
+            tie_game = False
+            for row in board:
+                for cell in row:
+                    if cell == " ":
+                        tie_game = True
+            # завершение цикла в случае ничьей
+            if not tie_game:
+                break
+            player = "O" if player == "X" else "X"
+        restart = input("Сыграть еще раз? (y/n) ")
+        if restart.lower() != "y":
+            break
+tic_tac_toe()
